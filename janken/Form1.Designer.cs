9749dc0b34574f1ebc4fe7b44226dec1ce68f8a8
たけだ <ichigo_happy@ezweb.ne.jp>
2020-01-22 11:42:33 +0900

    first commit

---
+++ janken/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace janken
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Tomato;
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.button1.Location = new System.Drawing.Point(31, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 108);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "グー";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.button2.Location = new System.Drawing.Point(279, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(239, 108);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "チョキ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gold;
+            this.button3.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.button3.Location = new System.Drawing.Point(536, 54);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(236, 108);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "パー";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.label1.Location = new System.Drawing.Point(223, 287);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 40);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.label2.Location = new System.Drawing.Point(602, 287);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 27);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ janken/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace janken
+{
+    public partial class Form1 : Form
+    {
+        public int choice;
+        public int player2 = 2; 
+        private void battle()
+        {
+            if ((player2-choice +3) % 3 == 0)
+            {
+                label1.Text = "あいこ";
+            }
+            else if ((player2-choice +3) % 3 == 1)
+            {
+                label1.Text = "あなたのかち";
+            }
+            else if ((player2-choice+3) % 3 == 2)
+            {
+                label1.Text = "あなたのまけ";
+            }
+            switch (player2)
+            {
+                case 1: label2.Text = "相手:グー";break;
+                case 2: label2.Text = "相手:チョキ";break;
+                case 3: label2.Text = "相手:パー";break;
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            choice = 1;
+            battle();
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            choice = 2;
+            battle();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            choice = 3;
+            battle();
+        }
+
+
+    }
+}
