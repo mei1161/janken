0d303e9834320abaafe6569700cc8f6994199c35
たけだ <ichigo_happy@ezweb.ne.jp>
2020-01-28 12:46:28 +0900

    fix code

--- janken/ConfigForm.Designer.cs
+++ janken/ConfigForm.Designer.cs
@@ -32,11 +32,13 @@
             this.btn_sender = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_recever
             // 
-            this.btn_recever.Location = new System.Drawing.Point(177, 140);
+            this.btn_recever.Location = new System.Drawing.Point(109, 56);
             this.btn_recever.Name = "btn_recever";
             this.btn_recever.Size = new System.Drawing.Size(110, 23);
             this.btn_recever.TabIndex = 0;
@@ -46,7 +48,7 @@
             // 
             // btn_sender
             // 
-            this.btn_sender.Location = new System.Drawing.Point(452, 139);
+            this.btn_sender.Location = new System.Drawing.Point(505, 56);
             this.btn_sender.Name = "btn_sender";
             this.btn_sender.Size = new System.Drawing.Size(100, 23);
             this.btn_sender.TabIndex = 1;
@@ -56,27 +58,49 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(248, 200);
+            this.textBox1.Location = new System.Drawing.Point(616, 387);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(258, 52);
+            this.textBox1.Size = new System.Drawing.Size(146, 27);
             this.textBox1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(334, 318);
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 8F);
+            this.button1.Location = new System.Drawing.Point(704, 420);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(58, 18);
             this.button1.TabIndex = 3;
             this.button1.Text = "送信";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.label1.Location = new System.Drawing.Point(613, 370);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 14);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "相手のIPアドレス";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(545, 347);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "上のボタンでつながらない場合";
             // 
             // ConfigForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btn_sender);
@@ -94,5 +118,7 @@
         private System.Windows.Forms.Button btn_sender;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- janken/ConfigForm.cs
+++ janken/ConfigForm.cs
@@ -140,5 +140,6 @@ namespace mei1161
             }
             
         }
+
     }
 }
